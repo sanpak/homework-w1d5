dd0658148d082b343c13a46e51650397d6a2c23f
SANPAK Ng <pakeng369@gmail.com>
2017-07-07 00:36:49 -0400

    Added Queue

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -10,18 +10,37 @@ class Stack
   end
 
   def remove
-    raise "Stack is empty" if @stack.is_empty?
+    raise "Stack is empty" if @stack.length == 0
     @stack.pop
   end
 
   def show
-    @stack.dup
+    @stack
   end
 end
 stack = Stack.new
 stack.add("John")
 stack.add("Peter")
 stack.add("May")
+
+class Queue
+  def initialize
+    @stack = []
+  end
+
+  def enqueue(el)
+    @stack << el
+    el
+  end
+
+  def dequeue
+    @stack.shift
+  end
+
+  def show
+    @stack
+  end
+end
 
 
 class Node
